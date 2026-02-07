e9407bd878db40c7d6b6d20857301c3ef07d96bf
Sujit Subedi <sujitsubedi567@gmail.com>
2026-02-07 21:09:02 +0545

    add filter and pagination

--- Frontend/src/lib/api.ts
+++ Frontend/src/lib/api.ts
@@ -1,8 +1,9 @@
 
 import axios from "axios";
-import process from "process";
 
-const url = process.env.API_URL || "http://localhost:5000/api"
+
+
+const url = import.meta.env.VITE_API_URL || "http://localhost:5000/api"
 
 const api = axios.create({
   baseURL: `${url}/notes`, 
